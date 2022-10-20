4c86b5cfbc8e1c98949230466846b00b2259f314
brnvska <92807364+brnvska@users.noreply.github.com>
2022-10-20 11:26:25 +0300

    Update FlowerBucket.java

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -1,7 +1,7 @@
 package flower.store;
 
-import lombok.Getter;
-import lombok.Setter;
+// import lombok.Getter;
+// import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +11,15 @@ public class FlowerBucket {
 
     // method price
 
-    private List<FlowerPack> BucketOfFlowers = new ArrayList<FlowerPack>();
+    private List<FlowerPack> bucket = new ArrayList<FlowerPack>();
     public void add(FlowerPack flowerPack) {
-        this.BucketOfFlowers.add(flowerPack);
+        bucket.add(flowerPack);
     }
 
     public double getPrice() {
         double price = 0;
 
-        for (FlowerPack pack: BucketOfFlowers){
+        for (FlowerPack pack: bucket){
             price += pack.getPrice();
         }
         return price;
